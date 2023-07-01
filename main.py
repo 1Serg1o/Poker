f4c80fe998f7d8a36f644a5e8b780eea6746a939
Sergio <ilyin.sergiusz@gmail.com>
2023-07-01 18:07:02 +0200

    poker draft

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+colors = ["Hearts", "Tiles", "Clovers", "Pikes"]
+card_values = [str(x) for x in range(2, 11)] + ["A", "J", "Q", "K"]
+deck = []
+for color in colors:
+    for c_v in card_values:
+        deck.append((color, c_v))
+
+number_of_player = int(input("How many player in game ? "))
+player_names = [input(f'Name of {number + 1} player: ') for number in range(number_of_player)]
+# player_names = []
+# for number in range(number_of_player):
+#     player_names.append(input(f'Name of {number + 1} player: '))
+print(player_names)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def hand_draw():
+    players_hands = {}
+    players_hands["Table"] = [deck.pop(random.randint(0, len(deck) - 1)) for _ in range(5)]
+    for name in player_names:
+        players_hands[name] = [deck.pop(random.randint(0, len(deck) - 1)) for _ in range(2)]
+    return players_hands
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(hand_draw())
